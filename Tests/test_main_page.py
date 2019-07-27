4b1ff9746e508d4ae45af3759f25c01f8de9f5dd
Andrey Kovalenko <kovalenko.andrey.odessa@gmail.com>
2019-07-27 17:21:35 +0300

    Add LoginPage object Add Locators

--- Tests/test_main_page.py
+++ Tests/test_main_page.py
@@ -1,20 +1,17 @@
 from Pages.main_page import MainPage
 
 
-def go_to_login_page(browser):
-    link = browser.find_element_by_css_selector("#login_link")
-    link.click()
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
