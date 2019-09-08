8e267d66884dba9b86ef2f92bb675e5d027239d0
Andrey Kovalenko <kovalenko.andrey.odessa@gmail.com>
2019-09-08 20:30:54 +0300

    Add check for add to basket button name

--- Pages/product_page.py
+++ Pages/product_page.py
@@ -1,5 +1,3 @@
-from selenium.webdriver.common.by import By
-
 from .base_page import BasePage
 from .locators import ProductPageLocators
 
@@ -7,9 +5,10 @@ from .locators import ProductPageLocators
 class ProductPage(BasePage):
 
     def __init__(self, browser, url, timeout=10):
-        super().__init__(browser, url, timeout=10)
+        super().__init__(browser, url, timeout=timeout)
         self.item_price = "Your basket total is now "
         self.item_name = " has been added to your basket."
+        self.add_to_basket_text = "Add to basket"
 
     def item_should_be_in_basket(self):
         assert "has been added to your basket." in self.browser.find_element(
@@ -22,8 +21,7 @@ class ProductPage(BasePage):
                                          f"but '{found}' was found"
 
     def press_add_to_basket(self):
-        # link = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
-        link = self.browser.find_element(By.XPATH, "//button[contains(text(),'basket')]")
+        link = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         link.click()
 
     def should_not_be_success_message(self):
@@ -37,6 +35,9 @@ class ProductPage(BasePage):
     def should_be_add_to_basket_button(self):
         assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BUTTON), "Add to basket button is not " \
                                                                                    "presented "
+        found = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON).text
+        assert found == self.add_to_basket_text, f"Wrong name for 'Add to basket button'." \
+                                                 f"Should be '{self.add_to_basket_text}' but '{found}' was found"
 
     def should_be_price_on_the_page(self):
         assert self.is_element_present(*ProductPageLocators.PRICE), "Price is not presented"
--- Tests/test_product_page.py
+++ Tests/test_product_page.py
@@ -33,12 +33,6 @@ class TestUserAddToBasketFromProductPage:
 
     @pytest.mark.parametrize('link',
                              ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                              # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                              # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                              # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                              # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                              # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                              # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
                               pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo"
                                            "=offer7", marks=pytest.mark.xfail),
                               "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
