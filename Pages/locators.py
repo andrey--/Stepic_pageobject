fda760cb16d6dce9d5ca6b9aa83ecbbd636b6733
Andrey Kovalenko <kovalenko.andrey.odessa@gmail.com>
2019-09-14 21:43:50 +0300

    Make CSS selector more strict

--- Pages/locators.py
+++ Pages/locators.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
-    VIEW_BASKET_BUTTON = (By.CSS_SELECTOR, "span.btn-group a.btn.btn-default")
+    VIEW_BASKET_BUTTON = (By.CSS_SELECTOR, "span.btn-group>a.btn.btn-default")
     USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
